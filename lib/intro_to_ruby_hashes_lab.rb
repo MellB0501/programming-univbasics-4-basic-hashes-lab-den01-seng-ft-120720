9b0998e4746da80d31bef6ec2a3f7a8bb0c1c114
Mellissa Basham <mellbasham@gmail.com>
2020-11-21 00:16:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,14 @@ def new_hash
 end
 
 def my_hash
-  { :favorite_name => "Henry" }
+  { favorite_name: => "Henry" }
 end
 
 def pioneer
-  { :name => "Grace Hopper" }
+  { name: => "Grace Hopper" }
 end
 
 
 def id_hash_generator(number)
-  { :id number }
+  { id: => number }
 end
